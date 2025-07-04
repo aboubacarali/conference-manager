09997493128a553762cefa8b450de66280433216
Aboubacar Ali <amsaboubacar06@gmail.com>
2025-07-04 16:08:12 +0200

    Added action to asign speaker to a talk in AppService and action corresponding endpoint in AppController

---
+++ src/main/java/fr/formation/conferencemanager/controller/AppController.java
@@ -0,0 +1,22 @@
+package fr.formation.conferencemanager.controller;
+
+import fr.formation.conferencemanager.entity.Speaker;
+import fr.formation.conferencemanager.service.AppService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class AppController {
+    private final AppService appService;
+
+    @PostMapping("/speaker/{speakerId}/assign-talk/{talkId}")
+    public ResponseEntity<Speaker> addSpeakerToTalk(@PathVariable Long speakerId, @PathVariable Long talkId) {
+        return ResponseEntity.ok(appService.addSpeakerToTalk(speakerId, talkId));
+    }
+}
---
+++ src/main/java/fr/formation/conferencemanager/service/AppService.java
@@ -0,0 +1,29 @@
+package fr.formation.conferencemanager.service;
+
+import fr.formation.conferencemanager.entity.Speaker;
+import fr.formation.conferencemanager.entity.Talk;
+import fr.formation.conferencemanager.repository.ConferenceRepository;
+import fr.formation.conferencemanager.repository.SpeakerRepository;
+import fr.formation.conferencemanager.repository.TalkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AppService {
+    private final ConferenceRepository conferenceRepository;
+    private final TalkRepository talkRepository;
+    private final SpeakerRepository speakerRepository;
+
+    public Speaker addSpeakerToTalk(Long speakerId, Long talkId) {
+        Optional<Speaker> speakerToUpdate = speakerRepository.findById(speakerId);
+        Optional<Talk> talkToAdd = talkRepository.findById(talkId);
+        if (speakerToUpdate.isEmpty() || talkToAdd.isEmpty()) {
+            throw new RuntimeException("Speaker or talk doesn't exist");
+        }
+        speakerToUpdate.get().getTalks().add(talkToAdd.get());
+        return speakerRepository.save(speakerToUpdate.get());
+    }
+}
